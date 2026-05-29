1b36fcbf16929b1566049df14a6758fdc5104cd8
Shunichiro Nomura <shunichiro.nomura.1035@gmail.com>
2026-05-29 14:53:32 +0900

    Initial commit

---
+++ src/package_name_goes_here/__init__.py
@@ -0,0 +1,6 @@
+"""Package initialization module for package-name-goes-here."""
+
+
+def main() -> None:
+    """Print a greeting message."""
+    print("Hello from package-name-goes-here!")  # noqa: T201
